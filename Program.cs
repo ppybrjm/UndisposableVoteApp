e92ac02b96b03ad87aa37835ca66f21f6c220938
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-07-27 09:48:23 +0100

    Logging (Also WIP, doesn't work)

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using VoteAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 
