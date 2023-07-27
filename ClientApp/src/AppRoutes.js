cdadfa2339c41c9e2b9cb74ccb1767711968e190
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-07-27 10:21:40 +0100

    Removing some default React Components

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,6 +1,4 @@
-import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
 import { Vote } from "./components/Vote";
 import { VoteSetting  } from "./components/VoteSetting";
 
@@ -8,23 +6,15 @@ import { VoteSetting  } from "./components/VoteSetting";
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  },
-  {
-    path: '/vote',
     element: <Vote />
   },
   {
     path: '/voteset/3664120',
     element: <VoteSetting />
+  },
+  {
+    path: '/fetchdata',
+    element: <FetchData />
   }
 ];
 
