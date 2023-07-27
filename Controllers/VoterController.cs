51a027da71d9e4971eac17875822cbacf1eccbd2
ROBERT MACKAY <Robert.Mackay.1988@gmail.com>
2023-07-27 09:48:32 +0100

    API Endpoints

--- Controllers/VoterController.cs
+++ Controllers/VoterController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using VoteAPI.Models;
 using VoteAPI.Data;
@@ -10,15 +9,127 @@ namespace VoteAPI.Controllers {
     public class VoteController : ControllerBase {
         private readonly VoteApiContext _context;
 
-        public VoteController(VoteApiContext context) {
+        private readonly ILogger<VoteController> _logger;
+
+        public VoteController(VoteApiContext context, ILogger<VoteController> logger) {
             _context = context;
+            _logger = logger;
         }
 
-        // Get
-        [HttpGet]
-        public JsonResult Get() {
-            return new JsonResult(Ok());
+        private void rob_log(string debug_message) {
+            Console.WriteLine(debug_message); 
+            _logger.LogDebug(message: debug_message);
         }
-        
+
+        // GetAll
+        [HttpGet]
+        public JsonResult GetAll() {
+            rob_log("gettingAll");
+            var context_voter = _context.Voter.ToList();
+            var context_voting = _context.Voting.ToList();
+            return new JsonResult(Ok(new {context_voter, context_voting}));
+        }
+    
+        // OpenNewVote
+        [HttpPost]
+        public JsonResult OpenNewVote(Voter voter) {
+            rob_log("OpenNewVote");
+            var context_open_votes = _context.Voter
+                                    .Where(x => x.voteOpen == true)
+                                    .Select(x=>x)
+                                    .ToList();
+            int count = context_open_votes.Count();
+            if (count >= 1) {
+                string message = "OpenNewVote - Pole Already Open";
+                rob_log(message);
+                return new JsonResult(BadRequest(new {message, context_open_votes}));
+            }
+            _context.Voter.Add(voter);
+            _context.SaveChanges();
+            rob_log("OpenNewVote - Created Pole");
+            return new JsonResult(Ok(voter));
+        }
+
+
+        // GetMostRecentVote
+        [HttpGet]
+        public JsonResult GetMostRecentVote() {
+            rob_log("GetMostRecentVote");
+            var howManyPoles = _context.Voter.Count();
+            if (howManyPoles == 0) {
+                rob_log("GetMostRecentVote - No Poles");
+                return new JsonResult(Ok("No Poles"));
+            }
+            
+            var most_recent_pole = _context.Voter
+                                   .OrderBy(x => x.id)
+                                   .Last();
+
+            var voting_in_pole = _context.Voting.Where(v => v.voterId == most_recent_pole.id);
+            var aVoteCount = voting_in_pole.Count(v => v.vote.Equals('A'));
+            var bVoteCount = voting_in_pole.Count(v => v.vote.Equals('B'));
+            rob_log("GetMostRecentVote - Pole");
+            return new JsonResult(
+                Ok(
+                    new {most_recent_pole, aVoteCount, bVoteCount}
+                )
+            );
+        }
+
+        [HttpPost]
+        public JsonResult ClosePoll() {
+            rob_log("ClosePoll");
+            var most_recent_pole = _context.Voter
+                                   .Where(x => x.voteOpen == true)
+                                   .OrderBy(x => x.id)
+                                   .Last();
+            most_recent_pole.voteOpen = false;
+            _context.SaveChanges();
+            return new JsonResult(Ok("Pole Closed"));
+        }
+
+        // isActivePole
+        [HttpGet]
+        public JsonResult isActivePole() {
+            rob_log("isActivePole");
+            var most_recent_pole_count = _context.Voter
+                                        .Where(x => x.voteOpen == true)
+                                        .Count();
+            bool activePole = (most_recent_pole_count == 0) ? false : true; 
+            return new JsonResult(Ok(activePole));
+        }
+
+        // vote
+        [HttpPost]
+        public JsonResult Vote(Voting voting) {
+            rob_log("Vote");
+            var voterInDb = _context.Voter.Find(voting.voterId);
+            if(voterInDb == null) {
+                rob_log("No Vote In DB");
+                return new JsonResult(NotFound("No Vote in DB"));
+            }
+
+            var voteInDB = _context.Voting
+                            .Where(v => v.userId == voting.userId)
+                            .Where(v => v.voterId == voting.voterId)
+                            .Select(v => v);
+
+            var count = voteInDB.Count();
+            if (count == 0) {
+                _context.Voting.Add(voting);
+            } else {
+                voteInDB.First().vote = voting.vote;
+            }
+
+            if (voting.vote.Equals('A') || voting.vote.Equals('B')) {
+                _context.SaveChanges();
+                rob_log("Voted!");
+                return new JsonResult(Ok("Voted!"));
+            } else {
+                rob_log("Invalid Vote");
+                return new JsonResult(NotFound("Invalid Vote"));
+            }
+
+        }
     }
 }
